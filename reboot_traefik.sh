669bf0d2db5ad28ab67811ef4a8dd85f8c62c7d7
Mudit Gurjar <mudit182@gmail.com>
2022-07-09 13:19:26 +0530

    added script for restarting traefik as duckdns often fails to update certificates

---
+++ reboot_traefik.sh
@@ -0,0 +1,17 @@
+sudo docker-compose down -v
+
+sudo rm letsencrypt/acme.json
+
+sudo docker-compose up -d
+# sleep 1m
+# sudo docker-compose down
+# sudo docker-compose up -d
+# sleep 1m
+# sudo docker-compose down
+# sudo docker-compose up -d
+# sleep 1m
+# sudo docker-compose down
+# sudo docker-compose up -d
+# sleep 1m
+# sudo docker-compose down
+# sudo docker-compose up -d
